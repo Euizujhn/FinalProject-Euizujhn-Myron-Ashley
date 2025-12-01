79f9eb301677606ff45824ef732e160877b929e1
Euizujhn <euizujhn11@gmail.com>
2025-11-30 22:21:00 -0500

    Update SearcherGUI.java, more stylized GUI

--- src/main/java/org/example/SearcherGUI.java
+++ src/main/java/org/example/SearcherGUI.java
@@ -1,15 +1,15 @@
-package org.example;
+  package org.example;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -29,21 +29,55 @@ public class SearcherGUI extends Application {
         VBox parent = new VBox();
         Label MealTitle = new Label("The Meal Database");
         MealTitle.setFont(Font.font("Times New Roman", FontWeight.BOLD, 24));
+        parent.setAlignment(Pos.TOP_CENTER);
+        parent.setPrefSize(400,200);
         parent.getChildren().add(MealTitle);
-        HBox urlArea = new HBox(new Label("Enter Meal to Received Recipe"));
+        Label urlArea = new Label("Enter Meal to Received the Recipe");
+        urlArea.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 20));
         TextField textField = new TextField();
-        urlArea.getChildren().add(textField);
+        textField.setAlignment(Pos.CENTER);
         parent.getChildren().add(urlArea);
-        Button button = new Button("Find Recipe");
+        parent.getChildren().add(textField);
+
+
+        //CUSTOM GUI
+        BackgroundFill colors = new BackgroundFill(
+                Color.GOLD,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+        );
+        Background background = new Background(colors);
+        parent.setBackground(background);
 
         //Recipe Search Vbox
+        Button button = new Button("Find Recipe");
+        button.setPrefSize(150, 50);
         button.setOnAction(event -> {
             String input = textField.getText();
             VBox favoriteAddRoot = new VBox();
-            favoriteAddRoot.getChildren().add(new Label(RecipeFormat.IngredientsGUI(input)));
+            ScrollPane ThescrollPane = new ScrollPane();
+            ThescrollPane.setContent(favoriteAddRoot);
+            ThescrollPane.setFitToWidth(true);
+            Label TheIngredients = new Label(RecipeFormat.IngredientsGUI(input));
+            TheIngredients.setFont(Font.font("Arial",FontWeight.LIGHT,12));
+            TheIngredients.setWrapText(true);
+            favoriteAddRoot.getChildren().add(TheIngredients);
+
+            //CUSTOM GUI
+            BackgroundFill AddColor = new BackgroundFill(
+                    Color.ORANGE,
+                    CornerRadii.EMPTY,
+                    Insets.EMPTY
+            );
+            Background fillColor = new Background(AddColor);
+            favoriteAddRoot.setBackground(fillColor);
 
             //Add Favorite Button
-            favoriteAddRoot.getChildren().add(new Label("Would you like to add to favorites?"));
+            Label Space = new Label("");
+            favoriteAddRoot.getChildren().add(Space);
+            Label AskFavorite = new Label("Would you like to add to favorites?");
+            AskFavorite.setFont(Font.font("Arial",FontWeight.LIGHT,12));
+            favoriteAddRoot.getChildren().add(AskFavorite);
             Button button2 = new Button("Yes");
             button2.setOnAction(event2 -> {Favorite.add(RecipeFormat.IngredientsGUI(input)); stage.setScene(parent.getScene()); stage.show();});
             favoriteAddRoot.getChildren().add(button2);
@@ -66,6 +100,7 @@ public class SearcherGUI extends Application {
                 Button buttonBack = new Button("Go Back");
                 buttonBack.setOnAction(event3 -> {stage.setScene(parent.getScene()); stage.show();});
                 instructionRoot.getChildren().add(buttonBack);
+
                 BorderPane ScrollFunction = new BorderPane();
                 ScrollFunction.setCenter(scrollPane);
                 Scene scene = new Scene(ScrollFunction, 600, 300);
@@ -73,18 +108,31 @@ public class SearcherGUI extends Application {
                 stage.show();
             });
             favoriteAddRoot.getChildren().add(button4);
-            Scene scene = new Scene(favoriteAddRoot, 400, 300);
+            BorderPane ScrollFunction = new BorderPane();
+            ScrollFunction.setCenter(ThescrollPane);
+            Scene scene = new Scene(ScrollFunction, 600, 300);
             stage.setScene(scene);
             stage.show();
         });
 
         //Check Favorites Button
         Button button1 = new Button("Check Favorites");
+        button1.setPrefSize(150, 50);
         button1.setOnAction(event2 -> {
             VBox FavoriteCheckRoot = new VBox();
             ScrollPane scrollPane = new ScrollPane();
             scrollPane.setContent(FavoriteCheckRoot);
             scrollPane.setFitToWidth(true);
+
+            //CUSTOM GUI
+            BackgroundFill AddColor = new BackgroundFill(
+                    Color.ORANGE,
+                    CornerRadii.EMPTY,
+                    Insets.EMPTY
+            );
+            Background fillColor = new Background(AddColor);
+            FavoriteCheckRoot.setBackground(fillColor);
+
             if (Favorite.isEmpty()){
                 FavoriteCheckRoot.getChildren().add(new Label("Nothing has been added"));
                 Button button3 = new Button("Go Back");
