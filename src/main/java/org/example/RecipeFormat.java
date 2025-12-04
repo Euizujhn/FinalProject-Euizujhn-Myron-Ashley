6fe70abfbb56b6d95e982f72bb9009a428de89e7
Euizujhn <euizujhn11@gmail.com>
2025-12-04 12:45:26 -0500

    Update RecipeFormat.java, added Area, Meal Title, and Category Format

--- src/main/java/org/example/RecipeFormat.java
+++ src/main/java/org/example/RecipeFormat.java
@@ -18,6 +18,28 @@ public class RecipeFormat extends RecipeAPI {
     return null;
     }
 
+    //Ingredients Print
+    public static String Ingredients(String FoodKey){
+        try {
+            JSONObject FoodIngredients = getFoodInfo(FoodKey);
+            String empty = "";
+
+            for (int i = 1; i <= 20; i++){
+                String List = (String) FoodIngredients.get("strIngredient" + i);
+                String Measure = (String) FoodIngredients.get("strMeasure" + i);
+                String combine =  List + ": " + Measure;
+                System.out.println(combine);
+                if (List.equals(empty)){
+                    return combine;
+                }
+            }
+
+        } catch (Exception e) {
+            // e.printStackTrace();
+        }
+        return null;
+    }
+
 
     //Ingredients GUI Output
     public static String IngredientsGUI(String FoodKey){
@@ -25,13 +47,18 @@ public class RecipeFormat extends RecipeAPI {
             JSONObject FoodIngredients = getFoodInfo(FoodKey);
             String empty = "";
             ArrayList<String> IngredientList = new ArrayList<>();
+            String Area = (String) FoodIngredients.get("strArea");
+            String FullMeal = (String) FoodIngredients.get("strMeal");
+            String Category = (String) FoodIngredients.get("strCategory");
+            String Origin = "Ethnicity : " + Area;
+            String Type = "Type: " + Category;
             for (int i = 1; i <= 20; i++) {
                 String List = (String) FoodIngredients.get("strIngredient" + i);
                 String Measure = (String) FoodIngredients.get("strMeasure" + i);
                 String combine =  List + ": " + Measure + "\n";
                 IngredientList.add(combine);
                 if (List.equals(empty)){
-                    return FoodKey + "\n" + String.valueOf(IngredientList);
+                    return FullMeal + "\n" + Origin + "\n" + Type + "\n" + "\n" + String.valueOf(IngredientList);
                 }
             }
         } catch (Exception e) {
